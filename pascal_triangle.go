1b1198f6f59a2dffdbbd66c450ee8ad2959cfd1a
Marcus Martina <mmartina@xebia.com>
2025-03-14 19:46:34 +0100

    minor

--- pascal_triangle.go
+++ pascal_triangle.go
@@ -5,7 +5,7 @@ func triangleRow(n int) []int64 {
 		return []int64{1}
 	}
 	var previousRow = triangleRow(n - 1)
-	row := make([]int64, 0, n)
+	row := make([]int64, 0, n+1)
 	row = append(row, 1)
 	for col := 1; col < n; col++ {
 		row = append(row, previousRow[col-1]+previousRow[col]+1)
